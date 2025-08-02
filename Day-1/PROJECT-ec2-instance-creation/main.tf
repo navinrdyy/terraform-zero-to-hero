9b48e3617d187d679a5badc93f5d1ebcbc57f388
navinrdyy <34156887+navinrdyy@users.noreply.github.com>
2025-08-02 09:21:04 +0000

    Modified main.tf file

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "test" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
 }
