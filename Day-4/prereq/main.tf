043c2fbde3310e9829f27bd71bf669f9cc38536d
navinrdyy <34156887+navinrdyy@users.noreply.github.com>
2025-09-02 10:11:17 +0000

    Adjusted main.tf and jenkinsfile to use parameters

--- Day-4/prereq/main.tf
+++ Day-4/prereq/main.tf
@@ -1,21 +1,33 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_s3" {
-  bucket = "terraform_s3"
+variable "s3_bkt_name" {
+  type        = string
+  description = "AWS S3 Bucket Name"
+  default     = "test"
+}
+
+variable "dynamo_tbl_name" {
+  type        = string
+  description = "AWS Dynamo Table Name"
+  default     = ""
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.s3_bkt_name
+
   tags = {
     Name        = "My s3 Dev bucket"
     Environment = "Dev"
   }
-  
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name             = "terraform_lock"
-  hash_key         = "LockID"
-  billing_mode     = "PAY_PER_REQUEST"
-  
+resource "aws_dynamodb_table" "dynamo_table" {
+  name         = var.dynamo_tbl_name
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
   attribute {
     name = "LockID"
     type = "S"
